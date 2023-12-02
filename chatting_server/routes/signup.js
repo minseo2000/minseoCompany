370a81e1de3facbc590b3b85a0e9041e28d401df
minseo2000 <qlqlxks123@naver.com>
2023-12-03 05:58:21 +0900

    updatE

--- chatting_server/routes/signup.js
+++ chatting_server/routes/signup.js
@@ -6,8 +6,9 @@ const sha = require('sha256');
 const {queryDatabase} = require('./db');
 const { generatePrimeSync } = require('crypto');
 
+/*
 router.post('/signup', async(req, res) => {
-    
+
     const id = req.body.id;
     const password = sha(req.body.password);
 
@@ -17,20 +18,20 @@ router.post('/signup', async(req, res) => {
                 const result = await queryDatabase(query, {
                     id : id,
                     password : password,
-                    
+
                 })
 
-                
-                
+
+
                 res.status(200).json({message:'성공!'});
             }
 
-    
+
     catch (error) {
         console.error(error);
         res.status(500).json();
     }
 });
-
+*/
 
 module.exports = router;
