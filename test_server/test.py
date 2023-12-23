a8f7b32e9940067179f1b110bfe92bde53b9bb48
minseo2000 <qlqlxks123@naver.com>
2023-12-24 00:26:38 +0900

    update

--- test_server/test.py
+++ test_server/test.py
@@ -8,4 +8,4 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=50000)
+    app.run(host='0.0.0.0')
--- test_server/wsgi.py
+++ test_server/wsgi.py
@@ -1,5 +1,5 @@
 # CODE
-from test_server.test import app
+from test import app
 
 if __name__ == "__main__":
     app.run()
