568a59336b6b23e66b8d511c63fcbd06427329ea
minseo2000 <qlqlxks123@naver.com>
2023-12-31 01:01:04 +0900

    updaet

--- deploy/_deploy_minseo_company_web/app.py
+++ deploy/_deploy_minseo_company_web/app.py
@@ -4,7 +4,7 @@ from flask import render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
-app = CORS(app)
+CORS(app)
 # app = Flask(__name__, template_folder='templates')
 
 FLUTTER_WEB_APP = 'templates'
